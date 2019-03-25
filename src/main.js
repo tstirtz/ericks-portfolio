28448fa1dbbd3241219f05c19326eb56fb91061b
Tyler Stirtz <tyler.stirtz@gmail.com>
2019-03-24 21:25:00 -0400

    add vue router and include in project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { store } from './store'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: routes
+})
 
 new Vue({
   store: store,
   render: h => h(App),
+  router: router
 }).$mount('#app')
